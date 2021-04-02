0e08ea9cadee950afa1049b74126edb49d4abddf
ezBrecher <ebrecher@u.rochester.edu>
2021-04-02 13:18:12 -0600

    Initial commit + navigation setup additions.

---
+++ app/src/main/java/com/ebrecher/p2/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.ebrecher.p2.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/ebrecher/p2/ui/main/WelcomeFragment.kt
@@ -0,0 +1,30 @@
+package com.ebrecher.p2.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ebrecher.p2.R
+
+class WelcomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = WelcomeFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.welcome_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
